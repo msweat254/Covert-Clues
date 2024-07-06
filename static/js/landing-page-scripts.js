5591ea6674f28a39bcf1161c1b8ebb717875ed59
msweation <michael.sweat@ionsolar.com>
2024-07-06 14:03:41 -0700

    Link to create game attached to host game button

--- static/js/landing-page-scripts.js
+++ static/js/landing-page-scripts.js
@@ -5,9 +5,4 @@ document.addEventListener('DOMContentLoaded', function() {
     joinGameButton.addEventListener('click', function() {
         joinGameForm.classList.toggle('hidden');
     });
-
-    // Host game button doesn't do anything yet
-    document.getElementById('host-game-button').addEventListener('click', function() {
-        alert('Host game functionality coming soon!');
-    });
 });
